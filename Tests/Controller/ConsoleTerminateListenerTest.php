071d01232beeb14dcbcb265b952ad62b690d576d
kodingkittens <philippe@kodingkittens.com>
2015-04-19 16:10:32 +0200

    removed default views

--- Tests/Controller/ConsoleTerminateListenerTest.php
+++ Tests/Controller/ConsoleTerminateListenerTest.php
@@ -4,13 +4,13 @@ namespace KK\Labs\ChuckConsoleBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class ConsoleTerminateListenerTest extends WebTestCase
 {
     public function testIndex()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+        $crawler = $client->request('GET', '/');
 
         $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
     }
