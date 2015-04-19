ce461d67c671f04f916c58dc51d356fe955bb526
kodingkittens <philippe@kodingkittens.com>
2015-04-19 19:40:08 +0200

    fixes for InSight

--- EventListener/ConsoleTerminateListener.php
+++ EventListener/ConsoleTerminateListener.php
@@ -2,7 +2,6 @@
 
 namespace KK\Labs\ChuckConsoleBundle\EventListener;
 
-use GuzzleHttp\Client;
 use KK\Labs\ChuckConsoleBundle\ChuckFactService\ChuckAPIService;
 use Symfony\Component\Console\Event\ConsoleTerminateEvent;
 
