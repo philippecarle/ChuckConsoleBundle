b3ec47fb255f35d350e6b2080651f432504d3b66
kodingkittens <philippe@kodingkittens.com>
2015-09-26 21:13:06 +0200

    better tests

--- Tests/ChuckFactService/ChuckFactServiceTest.php
+++ Tests/ChuckFactService/ChuckFactServiceTest.php
@@ -6,14 +6,30 @@ use KK\Labs\ChuckConsoleBundle\ChuckFactService\ChuckAPIService;
 
 class ChuckFactServiceTest extends \PHPUnit_Framework_TestCase
 {
-	public function testGetFact()
+	public function testGetCustomFact()
 	{
 		$firstName = substr(str_shuffle(MD5(microtime())), 0, 10);
 		$lastName = substr(str_shuffle(MD5(microtime())), 0, 10);
 
-		$chuck = new ChuckAPIService($firstName, $lastName, 5);
-		$fact = $chuck->getFact();
-
-		$this->assertStringMatchesFormat("%S$firstName%S$lastName%S", $fact);
+		$this->assertStringMatchesFormat("%S$firstName%S$lastName%S", $this->getFacts($firstName, $lastName));
 	}
+
+
+	public function testGetChuckFact()
+	{
+		$this->assertStringMatchesFormat("%SChuck%SNorris%S", $this->getFacts());
+	}
+
+	private function getFacts($firstName = 'Chuck', $lastName = 'Norris')
+	{
+		$chuck = new ChuckAPIService($firstName, $lastName, 5);
+
+		$facts = "";
+		for($i = 0; $i < 5; $i++ ) {
+			$facts.= $chuck->getFact();
+		}
+
+		return $facts;
+	}
+
 }
